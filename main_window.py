ca9b7abc603d8006390d476a450f5e2d535fab67
miguelcapaz7 <55458822+miguelcapaz7@users.noreply.github.com>
2020-04-27 17:15:35 -0700

    Template for Main Window

---
+++ main_window.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+class MainWindow(Frame):
+    """ Layout for the Main Window """
+
+    def __init__(self, parent, controller):
+        """ Initialize Main Application """
+        Frame.__init__(self, parent)
+
+        parent.title('Video Downloader')
+
+        # add a menu here
+
+        # define frames here
+
+        # define labels here
+
+        # define entry box here
+
+        # (optional) we can add a scrollbar for listbox here
+
+        # define buttons here
+
+    # methods are defined here (outside of __init__() )
+    def method1(self):
+        pass
