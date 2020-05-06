74ca62bc7e46493b2b83ee3e4ee6534eebd83bd6
apu-pixel <62687887+apu-pixel@users.noreply.github.com>
2020-05-06 16:15:54 -0700

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -20,34 +20,50 @@ class MainWindow(Frame):
         file_menu.add_command(label="Exit", command=self.quit)
 
         # define frames here
-        left_frame = Frame(parent)
-        left_frame.pack(side=LEFT)
-        right_frame = Frame(parent)
-        right_frame.pack(side=RIGHT)
-        # define labels here
-        enter_label = Label(left_frame, text="Enter Link: ")
-        enter_label.pack(side=LEFT)
+        win = Frame(parent)
+        win.pack(side="top", fill="both", expand=True)
+        win.grid_columnconfigure(2, weight=1)
+        win.grid_rowconfigure(2, weight=1)
+       # left_frame = Frame(parent)
+       # left_frame.pack(side=LEFT)
+        #right_frame = Frame(parent)
+        #right_frame.pack(side=RIGHT)
+        # define labels
+        entrystuff = Frame(win)
+        entrystuff.grid(row=0,column=0)
+
+        enter_label = Label(entrystuff, text="Enter Link: ")
+        enter_label.pack(side=TOP)
         # define entry box here
-        self.entry_link = Entry(left_frame, width="40")
-        self.entry_link.pack(side=LEFT)
+        self.entry_link = Entry(entrystuff, width="40")
+        self.entry_link.pack()
+
+        liststuff = Frame(win)
+        liststuff.grid(row=2)
 
         # (optional) we can add a scrollbar for listbox here
-        self.listbox = Listbox(right_frame, height="30", width="50")
+        self.listbox = Listbox(liststuff, height="30", width="50")
         self.listbox.pack()
+
+        buttonframe= Frame(win)
+        buttonframe.grid(row=1,column=0)
+        buttonframe.grid_columnconfigure(4,weight=1)
+
         # define buttons here
-        download_button = Button(left_frame, text="Download", fg="white", bg="black")
+        download_button = Button(buttonframe, text="Download", fg="white", bg="black")
         download_button.bind("<Button-1>", contoller.download_callback)
-        download_button.pack(side=TOP)
+        download_button.grid(row=0,column=0,padx=1)
 
-        rename_button = Button(left_frame, text="Rename", fg="white", bg="black")
-       #rename_button.bind("<Button-1>", contoller.rename_callback)
-        rename_button.pack(side=BOTTOM)
+        rename_button = Button(buttonframe, text="Rename", fg="white", bg="black")
+        rename_button.bind("<Button-1>", contoller.rename_window)
+        #rename_button.bind("<Button-1>", contoller.download_callback)
+        rename_button.grid(row=0,column=1,padx=1)
 
-        play_button = Button(left_frame, text="Play", fg="white", bg="green")
-        play_button.pack(side=BOTTOM)
+        play_button = Button(buttonframe, text="Play", fg="white", bg="green")
+        play_button.grid(row=0,column=2,padx=1)
 
-        delete_button = Button(left_frame, text="Delete", fg="white", bg="red")
-        delete_button.pack(side=BOTTOM)
+        delete_button = Button(buttonframe, text="Delete", fg="white", bg="red")
+        delete_button.grid(row=0,column=3,padx=1)
 
     # methods are defined here (outside of __init__() )
     def insert_to_listbox(self, titles):
