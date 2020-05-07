6b7c58590ca82cd803dbc3f604f1a54dc6b42c71
miguelcapaz7 <55458822+miguelcapaz7@users.noreply.github.com>
2020-05-07 15:15:14 -0700

    Fixed pady property

--- rename_window.py
+++ rename_window.py
@@ -18,16 +18,16 @@ class RenameWindow(Frame):
 
         # define label that has the title you want to rename
         title_label = Label(left_frame, text=title, bg='gray37', fg="white")
-        title_label.grid(row=0, padY=10)
+        title_label.grid(row=0, pady=10)
 
         # define entry box
         self.entry_name = Entry(right_frame, width="40")
-        self.entry_name.grid(row=1, column=0 ,padX=5 , padY=5)
+        self.entry_name.grid(row=1, column=0 ,padx=5 , pady=5)
 
         # define save button
         save_button = Button(right_frame, text="Save", fg="white", bg="RoyalBlue1")
         save_button.bind("<Button-1>", contoller.update_title)
-        save_button.grid(row=1,column=1, padX=5, padY=5)
+        save_button.grid(row=1,column=1, padx=5, pady=5)
 
     def get_form_data(self):
         """ Return a dictionary of form field values with updated title """
