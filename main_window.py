adbd726504ff997ef5b447c2c84972c88f57732e
apu-pixel <62687887+apu-pixel@users.noreply.github.com>
2020-05-12 18:41:59 -0700

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 
 class MainWindow(Frame):
@@ -21,29 +22,29 @@ class MainWindow(Frame):
         file_menu.add_command(label="Exit", command=self.quit)
 
         # define frames here
-        left_frame = Frame(parent, bg='gray37')
+        left_frame = Frame(parent, background='gray37')
         left_frame.pack(side=TOP)
-        right_frame = Frame(parent, bg='gray15')
+        right_frame = Frame(parent, background='gray15')
         right_frame.pack()
         bottom_frame = Frame(parent)
         bottom_frame.pack(side=BOTTOM)
-        space_label6 = Label(bottom_frame, text="", bg='gray15')
+        space_label6 = Label(bottom_frame, text="", background='gray15')
         space_label6.pack(side=BOTTOM)
         # bottom_frame = Frame(parent)
         # bottom_frame
         # define labels here
-        space_label7 = Label(left_frame, text="", bg='gray37')
+        space_label7 = ttk.Label(left_frame, text="", background='gray37')
         space_label7.pack(side=TOP)
-        enter_label = Label(left_frame, text="Enter Link: ", bg='gray37', fg="white")
+        enter_label = ttk.Label(left_frame, text="Enter Link: ", background='gray37', foreground="white")
         enter_label.pack(side=LEFT)
         # define entry box here
         self.entry_link = Entry(left_frame, width="40")
         self.entry_link.pack(side=LEFT)
-        space_label = Label(left_frame, text="    ", bg='gray37')
+        space_label = ttk.Label(left_frame, text="    ", background='gray37')
         space_label.pack(side=LEFT)
         # space_label5 = Label(left_frame, text="lol   ")
         # space_label5.pack(side=BOTTOM)
-        space_label5 = Label(right_frame, text="", bg='gray15')
+        space_label5 = ttk.Label(right_frame, text="", background='gray15')
         space_label5.pack(side=TOP)
 
         # (optional) we can add a scrollbar for listbox here
@@ -51,30 +52,30 @@ class MainWindow(Frame):
         self.listbox.pack(side=TOP)
 
         # define buttons here
-        download_button = Button(left_frame, text="Download", fg="white", bg="RoyalBlue1")
+        download_button = ttk.Button(left_frame, text="Download")
         download_button.bind("<Button-1>", contoller.download_callback)
         download_button.pack(side=LEFT)
-        space_label2 = Label(left_frame, text=" ", bg='gray37')
+        space_label2 = ttk.Label(left_frame, text=" ", background='gray37')
         space_label2.pack(side=LEFT)
 
-        space_label4 = Label(right_frame, text=" " * 25, bg='gray15')
+        space_label4 = ttk.Label(right_frame, text=" " * 25, background='gray15')
         space_label4.pack(side=TOP)
-        rename_button = Button(right_frame, text="Rename", fg="white", bg="RoyalBlue1")
+        rename_button = ttk.Button(right_frame, text="Rename")
         rename_button.bind("<Button-1>", contoller.rename_window_popup)
         rename_button.pack(side=LEFT)
 
-        space_label3 = Label(right_frame, text=" " * 25, bg='gray15')
+        space_label3 = ttk.Label(right_frame, text=" " * 25, background='gray15')
         space_label3.pack(side=LEFT)
 
-        play_button = Button(right_frame, text="Play", fg="white", bg="green")
+        play_button = ttk.Button(right_frame, text="Play")
         play_button.bind("<Button-1>", contoller.play_video)
         play_button.pack(side=LEFT)
 
-        delete_button = Button(right_frame, text="Delete", fg="white", bg="red")
+        delete_button = ttk.Button(right_frame, text="Delete")
         delete_button.bind("<Button-1>", contoller.delete_callback)
         delete_button.pack(side=RIGHT)
 
-        space_label5 = Label(right_frame, text="" * 25, bg='gray15')
+        space_label5 = ttk.Label(right_frame, text="" * 25, background='gray15')
         space_label5.pack(side=BOTTOM)
 
     # methods are defined here (outside of __init__() )
