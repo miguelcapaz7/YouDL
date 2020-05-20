ddcadbb69ef91d46a1f604d2e9da45e3340d6026
miguelcapaz7 <55458822+miguelcapaz7@users.noreply.github.com>
2020-05-19 17:39:50 -0700

    Updated Details window

--- details_window.py
+++ details_window.py
@@ -16,24 +16,41 @@ class DetailsWindow(Frame):
         self.mid_frame = Frame(self.master, bg='gray15')
         self.mid_frame.grid(row=1, padx=30, pady=10)
 
-        Label(self.mid_frame, text='Title:', bg='gray15', fg='white', font=myFont)\
-            .grid(row=0, column=0, sticky=E, padx=5, pady=5)
-        Label(self.mid_frame, text='Author:', bg='gray15', fg='white', font=myFont)\
-            .grid(row=1, column=0, sticky=E, padx=5, pady=5)
-        Label(self.mid_frame, text='Resolution:', bg='gray15', fg='white', font=myFont)\
-            .grid(row=2, column=0, sticky=E, padx=5, pady=5)
-        Label(self.mid_frame, text='Frame Rate:', bg='gray15', fg='white', font=myFont)\
-            .grid(row=3, column=0, sticky=E, padx=5, pady=5)
-        Label(self.mid_frame, text='File Location:', bg='gray15', fg='white', font=myFont)\
-            .grid(row=4, column=0, sticky=E, padx=5, pady=5)
+        file, extension = video_dict['filename'].split(".")
+        if extension == "mp3":
+            Label(self.mid_frame, text='Title:', bg='gray15', fg='white', font=myFont)\
+                .grid(row=0, column=0, sticky=E, padx=5, pady=5)
+            Label(self.mid_frame, text='Author:', bg='gray15', fg='white', font=myFont)\
+                .grid(row=1, column=0, sticky=E, padx=5, pady=5)
+            Label(self.mid_frame, text='File Location:', bg='gray15', fg='white', font=myFont)\
+                .grid(row=4, column=0, sticky=E, padx=5, pady=5)
 
-        Label(self.mid_frame, text=video_dict['title'], bg='gray15', fg='white', font=myFont)\
-            .grid(row=0, column=1, sticky=W, padx=5, pady=5)
-        Label(self.mid_frame, text=video_dict['author'], bg='gray15', fg='white', font=myFont)\
-            .grid(row=1, column=1, sticky=W, padx=5, pady=5)
-        Label(self.mid_frame, text=video_dict['resolution'], bg='gray15', fg='white', font=myFont)\
-            .grid(row=2, column=1, sticky=W, padx=5, pady=5)
-        Label(self.mid_frame, text=video_dict['frame_rate'] + "fps", bg='gray15', fg='white', font=myFont)\
-            .grid(row=3, column=1, sticky=W, padx=5, pady=5)
-        Label(self.mid_frame, text=video_dict['pathname'] + video_dict['filename'], bg='gray15',
-              fg='white', font=myFont).grid(row=4, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['title'], bg='gray15', fg='white', font=myFont)\
+                .grid(row=0, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['author'], bg='gray15', fg='white', font=myFont)\
+                .grid(row=1, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['pathname'] + video_dict['filename'], bg='gray15',
+                  fg='white', font=myFont).grid(row=4, column=1, sticky=W, padx=5, pady=5)
+
+        else:
+            Label(self.mid_frame, text='Title:', bg='gray15', fg='white', font=myFont) \
+                .grid(row=0, column=0, sticky=E, padx=5, pady=5)
+            Label(self.mid_frame, text='Author:', bg='gray15', fg='white', font=myFont) \
+                .grid(row=1, column=0, sticky=E, padx=5, pady=5)
+            Label(self.mid_frame, text='Resolution:', bg='gray15', fg='white', font=myFont) \
+                .grid(row=2, column=0, sticky=E, padx=5, pady=5)
+            Label(self.mid_frame, text='Frame Rate:', bg='gray15', fg='white', font=myFont) \
+                .grid(row=3, column=0, sticky=E, padx=5, pady=5)
+            Label(self.mid_frame, text='File Location:', bg='gray15', fg='white', font=myFont) \
+                .grid(row=4, column=0, sticky=E, padx=5, pady=5)
+
+            Label(self.mid_frame, text=video_dict['title'], bg='gray15', fg='white', font=myFont) \
+                .grid(row=0, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['author'], bg='gray15', fg='white', font=myFont) \
+                .grid(row=1, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['resolution'], bg='gray15', fg='white', font=myFont) \
+                .grid(row=2, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['frame_rate'] + "fps", bg='gray15', fg='white', font=myFont) \
+                .grid(row=3, column=1, sticky=W, padx=5, pady=5)
+            Label(self.mid_frame, text=video_dict['pathname'] + video_dict['filename'], bg='gray15',
+                  fg='white', font=myFont).grid(row=4, column=1, sticky=W, padx=5, pady=5)
